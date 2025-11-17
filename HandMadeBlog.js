a35665b1453ca1f0f85cac4bc6d11cba2ccb8237
MyLilla <ninakarina.iok@gmail.com>
2025-11-17 22:01:30 +0100

    reading from Json, fdaptive for pc

--- HandMadeBlog.js
+++ HandMadeBlog.js
@@ -1,27 +1,47 @@
-// menu btns
-let buttons = document.querySelectorAll(".ringBtn");
-buttons.forEach(btn => {
-    btn.onclick = function (e) {
-        if (!e.target.classList.contains("active")) {
-            btn.classList.add("active");
-
-        } else {
-            btn.classList.remove("active");
+// reading
+fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+        for (item of data) {
+            document.getElementById("cards")
+                .append(createCard(item));
         }
+        modalLouded()
+    })
+    .catch(err => console.error(err));
+
+function createCard(item) {
+
+    const tpl = document.getElementById("cardTemplate")
+        .content.cloneNode(true);
+
+    tpl.querySelector(".photo").src = item.imgPreview;
+    tpl.querySelector("h2").textContent = item.title;
+    tpl.querySelector("p").textContent = item.description;
+
+    for (let i = 0; i < item.materials.length; i++) {
+
+        let icon = document.createElement("img");
+        icon.className = "icon"
+        tpl.querySelector(".cardMenu").prepend(icon)
+        tpl.querySelector(".icon").src = item.materials[i]
     }
-});
+    if (!item.available) tpl.querySelector("button").innerHTML = "not available"
+
+    tpl.querySelector(".price").textContent = item.price
+
+    return tpl;
+}
 
 // modal
 let modal = document.getElementById('modal');
 let closeBtn = document.getElementById('closeModal');
 
-document.addEventListener('DOMContentLoaded', () => {
+function modalLouded() {
     document.querySelectorAll('.openModal').forEach(button => {
+
         button.addEventListener('click', function (e) {
-
-
             let card = e.target.closest('.cardMenu');
-            console.log(card)
 
             let price = card.querySelector('.price').textContent.trim();
             document.querySelector('.sellerPrice')
@@ -31,9 +51,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 .textContent = price + " €";
             modal.classList.add('show');
 
-        });
-    });
-});
+        })
+    })
+}
 
 // close modal
 closeBtn.addEventListener('click', () => {
@@ -43,23 +63,28 @@ modal.addEventListener('click', (e) => {
     if (e.target === modal) {
         modal.classList.remove('show');
     }
-});
-
-// open more
-let geleries = document.querySelectorAll(".gelery")
-document.querySelector(".more button").addEventListener("click", function (e) {
-    for (let gelery of geleries) {
-        if (!gelery.classList.contains("show")) {
-            gelery.classList.add("show")
-            return
-        }
-    }
 })
 
-// not active
-
+// in process
 let logInBtn = document.querySelector(".login").addEventListener("click",
     function () {
         alert("this function in in process...")
     }
 )
+
+document.querySelector(".more button").addEventListener("click", function (e) {
+    alert("this function in in process...")
+})
+
+// menu btns
+let buttons = document.querySelectorAll(".ringBtn");
+buttons.forEach(btn => {
+    btn.onclick = function (e) {
+        if (!e.target.classList.contains("active")) {
+            btn.classList.add("active");
+            alert("this function in in process...")
+        } else {
+            btn.classList.remove("active");
+        }
+    }
+})
--- README.md
+++ README.md
@@ -1,18 +1,17 @@
-[![Typing SVG](https://readme-typing-svg.herokuapp.com?font=Fira+Code&pause=1000&width=435&lines=HandMade blog)](https://git.io/typing-svg)
 
 <h1><a> HandMade blog </a></h1>
 Demo project fof skill demonstration  <br>
 !Under development
 
 <h3> <a>About:</a> </h3>
-My own blog with handmade products
-For now 
-<h3> <a>Plans:</a></h3>
+My own blog with handmade products 
+<br> <br>
 
+For now possible: <br>
+✅ to see all items <br>
+✅ to connect with me <br>
 
-<h3> <a>Link:</a></h3>
-
-```localhost:1111```
+[```Link HandMadeBlog```](https://mylilla.github.io/HandMadeBlog/)
 
 <h3> <a>Technology</a></h3>
 
@@ -22,7 +21,9 @@ For now
 
 <h3> <a>Possible future:</a></h3>
 
-⚪ Закрытие модального окна (открытие через bs)
-⚪ Додумать что в модальном окне
-⚪ как загухать картинки в liquibase 
-⚪ ВАЖНО, не забыть подправить картинки в changelogs перед заливкой на git
+⚪ Sorting products by category <br>
+⚪ Improve the modal window <br>
+⚪ Add a Spanish interface <br>
+⚪ <br>
+⚪ <br>
+⚪ <br>
